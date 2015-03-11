cb8f1e4e610adcb843a1de4d7ba64970f1e827d3
Josh Baker <joshbaker77@gmail.com>
2015-03-11 09:24:27 -0700

    Reduced size of js output by 10x. Updated GopherJS calls.

--- digitalrain.go
+++ digitalrain.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"github.com/gopherjs/gopherjs/js"
-	"log"
-	"math/rand"
-	"time"
+)
+
+const (
+	showBlueHeads = true
+	overlap       = 0 // 0-N, allowable drop overlaps for a column
+	highColor     = "#7bB5C8"
+	lowColor      = "#3b806d"
+	githubLink    = "http://github.com/tidwall/digitalrain"
+	level1Cols    = 40
+	level2Cols    = 60
 )
 
 func main() {
@@ -16,18 +21,18 @@ func main() {
 	js.Global.Get("document").Get("head").Call("appendChild", sheet)
 	js.Global.Get("document").Set("title", "whoa")
 	js.Global.Call("addEventListener", "load", func() {
-		rain1, err := NewDigitalRain(js.Global.Get("document").Get("body"), 60, 2, 8, 0.25)
+		rain1, err := NewDigitalRain(js.Global.Get("document").Get("body"), level2Cols, 2, 8, 0.25)
 		if err != nil {
-			log.Println(err.Error())
+			println(err.Error())
 			return
 		}
 		js.Global.Call("addEventListener", "resize", func() {
 			rain1.layout()
 		})
 
-		rain2, err := NewDigitalRain(js.Global.Get("document").Get("body"), 40, 2, 12, 1.0)
+		rain2, err := NewDigitalRain(js.Global.Get("document").Get("body"), level1Cols, 2, 12, 1.0)
 		if err != nil {
-			log.Println(err.Error())
+			println(err.Error())
 			return
 		}
 		js.Global.Call("addEventListener", "resize", func() {
@@ -36,23 +41,28 @@ func main() {
 	})
 }
 
-const (
-	showBlueHeads = true
-	//screenCols    = 65
-	overlap    = 0 // 0-N, allowable drop overlaps for a column
-	highColor  = "#7bB5C8"
-	lowColor   = "#3b806d"
-	githubLink = "http://github.com/tidwall/digitalrain"
-)
+type Duration float64
+
+const Second Duration = 1
+
+func itoa(i int) string {
+	return js.Global.Get("String").New(i).String()
+}
+func ftoa(f float64) string {
+	return js.Global.Get("String").New(f).String()
+}
+func randi() int {
+	return int(js.Global.Get("Math").Call("random").Float() * 2147483647.0)
+}
 
 type DigitalRain struct {
-	parent, canvas  js.Object
-	ctx             js.Object
+	parent, canvas  *js.Object
+	ctx             *js.Object
 	width, height   float64
 	ratio           float64
-	timestamp       time.Duration
-	highGlyphCanvas js.Object
-	lowGlyphCanvas  js.Object
+	timestamp       Duration
+	highGlyphCanvas *js.Object
+	lowGlyphCanvas  *js.Object
 	drops           []*waterDrop
 	linkover        bool
 	screenCols      int
@@ -61,7 +71,7 @@ type DigitalRain struct {
 	brightness      float64
 }
 
-func NewDigitalRain(parent js.Object, screenCols int, minSpeed int, maxSpeed int, brightness float64) (*DigitalRain, error) {
+func NewDigitalRain(parent *js.Object, screenCols int, minSpeed int, maxSpeed int, brightness float64) (*DigitalRain, error) {
 	rain := &DigitalRain{parent: parent}
 	rain.screenCols = screenCols
 	rain.minSpeed = minSpeed
@@ -73,7 +83,6 @@ func NewDigitalRain(parent js.Object, screenCols int, minSpeed int, maxSpeed int
 	return rain, nil
 }
 func (r *DigitalRain) start() error {
-	rand.Seed(time.Now().UnixNano())
 	var raf string
 	for _, s := range []string{"requestAnimationFrame", "webkitRequestAnimationFrame", "mozRequestAnimationFrame"} {
 		if js.Global.Get(s) != js.Undefined {
@@ -82,15 +91,15 @@ func (r *DigitalRain) start() error {
 		}
 	}
 	if raf == "" {
-		return errors.New("requestAnimationFrame is not available")
+		panic("requestAnimationFrame is not available")
 	}
 	defer r.layout()
 	count := 0
-	var f func(js.Object)
-	f = func(timestampJS js.Object) {
+	var f func(*js.Object)
+	f = func(timestampJS *js.Object) {
 		js.Global.Call(raf, f)
 		if count%2 == 0 {
-			r.loop(time.Duration(timestampJS.Float() * float64(time.Millisecond)))
+			r.loop(Duration(timestampJS.Float() / 1000))
 		}
 		count++
 	}
@@ -113,8 +122,8 @@ func (r *DigitalRain) layout() {
 	r.ctx = r.canvas.Call("getContext", "2d")
 	r.canvas.Set("width", r.width)
 	r.canvas.Set("height", r.height)
-	r.canvas.Get("style").Set("width", fmt.Sprintf("%.4fpx", r.width/r.ratio))
-	r.canvas.Get("style").Set("height", fmt.Sprintf("%.4fpx", r.height/r.ratio))
+	r.canvas.Get("style").Set("width", ftoa(r.width/r.ratio)+"px")
+	r.canvas.Get("style").Set("height", ftoa(r.height/r.ratio)+"px")
 	r.canvas.Get("style").Set("position", "absolute")
 	r.parent.Call("appendChild", r.canvas)
 	if r.highGlyphCanvas == nil {
@@ -124,13 +133,13 @@ func (r *DigitalRain) layout() {
 		r.lowGlyphCanvas = generateGlyphCanvas(lowColor)
 	}
 
-	r.canvas.Call("addEventListener", "click", func(ev js.Object) {
+	r.canvas.Call("addEventListener", "click", func(ev *js.Object) {
 		if r.overLink(ev.Get("x").Int(), ev.Get("y").Int()) {
 			js.Global.Set("location", githubLink)
 		}
 	})
 
-	r.canvas.Call("addEventListener", "mousemove", func(ev js.Object) {
+	r.canvas.Call("addEventListener", "mousemove", func(ev *js.Object) {
 		if r.overLink(ev.Get("x").Int(), ev.Get("y").Int()) {
 			r.canvas.Get("style").Set("cursor", "pointer")
 			r.linkover = true
@@ -154,16 +163,16 @@ type waterDrop struct {
 	speed   float64 // how many cells per second
 	glyphs  []int   // random glyph table
 	spedup  bool
-	created time.Duration
+	created Duration
 }
 
-func (r *DigitalRain) dropWaterAtCol(col int, speed float64, length int, start float64, created time.Duration) {
+func (r *DigitalRain) dropWaterAtCol(col int, speed float64, length int, start float64, created Duration) {
 	wd := waterDrop{}
 	wd.col = col
 	wd.speed = speed
 	wd.glyphs = make([]int, length)
 	for i := 0; i < length; i++ {
-		wd.glyphs[i] = rand.Int() % glyphsCount
+		wd.glyphs[i] = randi() % glyphsCount
 	}
 	r.drops = append(r.drops, &wd)
 
@@ -172,8 +181,8 @@ func (r *DigitalRain) dropWaterAtCol(col int, speed float64, length int, start f
 	wd.created = created
 }
 
-func (r *DigitalRain) dropRandomWaterDrop(timestamp time.Duration) {
-	col := rand.Int() % r.screenCols
+func (r *DigitalRain) dropRandomWaterDrop(timestamp Duration) {
+	col := randi() % r.screenCols
 	colcnt := 0
 	for _, drop := range r.drops {
 		if drop.col == col && int(drop.row)-len(drop.glyphs) < 0 {
@@ -183,9 +192,9 @@ func (r *DigitalRain) dropRandomWaterDrop(timestamp time.Duration) {
 			}
 		}
 	}
-	speed := float64(rand.Int()%(r.maxSpeed-r.minSpeed) + r.minSpeed)
-	length := rand.Int()%30 + 10
-	start := float64((rand.Int() % r.maxRows()) - r.maxRows()/2)
+	speed := float64(randi()%(r.maxSpeed-r.minSpeed) + r.minSpeed)
+	length := randi()%30 + 10
+	start := float64((randi() % r.maxRows()) - r.maxRows()/2)
 	r.dropWaterAtCol(col, speed, length, start, timestamp)
 }
 func (r *DigitalRain) maxRows() int {
@@ -201,7 +210,7 @@ func (r *DigitalRain) drawGlyphAt(nidx int, col int, row float64, brightness flo
 		r.drawGlyphElAt(r.highGlyphCanvas, nidx, col, row, brightness)
 	}
 }
-func (r *DigitalRain) drawGlyphElAt(glyphCanvas js.Object, nidx int, col int, row float64, brightness float64) {
+func (r *DigitalRain) drawGlyphElAt(glyphCanvas *js.Object, nidx int, col int, row float64, brightness float64) {
 	if brightness <= 0.05 {
 		return
 	}
@@ -225,7 +234,7 @@ func (r *DigitalRain) drawTitle(text string, color string, fontSize float64, y f
 	x := r.width - pad
 	y = r.height - pad - y
 	r.ctx.Call("save")
-	r.ctx.Set("font", fmt.Sprintf("%dpx Menlo, Consolas, Monospace, Helvetica, Arial, Sans-Serif", int(fontSize)))
+	r.ctx.Set("font", itoa(int(fontSize))+"px Menlo, Consolas, Monospace, Helvetica, Arial, Sans-Serif")
 	r.ctx.Set("textAlign", "right")
 	r.ctx.Set("lineWidth", 0)
 	r.ctx.Set("shadowColor", color)
@@ -250,7 +259,7 @@ func (r *DigitalRain) drawTitles() {
 
 }
 
-func (r *DigitalRain) loop(timestamp time.Duration) {
+func (r *DigitalRain) loop(timestamp Duration) {
 	if timestamp == 0 || r.timestamp == 0 {
 		r.timestamp = timestamp
 		return
@@ -264,19 +273,20 @@ func (r *DigitalRain) loop(timestamp time.Duration) {
 
 	var drops []*waterDrop
 	for _, drop := range r.drops {
+		var ri = randi()
 		if !drop.spedup {
-			if rand.Int()%250 == 0 {
-				drop.speed *= float64(rand.Int()%3) + 0.8
+			if ri%250 == 0 {
+				drop.speed *= float64(ri%3) + 0.8
 				drop.spedup = true
 			}
 		}
-		gbrightness := float64(r.brightness)
+		gbrightness := r.brightness
 		age := (timestamp - drop.created)
-		if age < time.Second {
-			gbrightness = float64(age) / float64(time.Second)
+		if age < Second {
+			gbrightness = float64(age / Second)
 		}
 
-		drop.row += float64(elapsed) / float64(time.Second) * drop.speed
+		drop.row += float64(elapsed) / float64(Second) * drop.speed
 		gl := len(drop.glyphs)
 
 		if int(drop.row)-gl > r.maxRows() {
@@ -288,11 +298,12 @@ func (r *DigitalRain) loop(timestamp time.Duration) {
 			gcount = len(drop.glyphs)
 		}
 		for i := 0; i < gcount; i++ {
+			var ri = randi()
 			glyph := drop.glyphs[i]
 			brightness := 1 - (float64(i) / float64(gcount))
 			// one in N change that the glyph will change, just because
-			if rand.Int()%50 == 0 {
-				glyph = rand.Int() % glyphsCount
+			if ri%50 == 0 {
+				glyph = ri % glyphsCount
 				drop.glyphs[i] = glyph
 			}
 			row := drop.row - float64(i)
@@ -310,7 +321,7 @@ const (
 	glyphFontSize = 86
 )
 
-func generateGlyphCanvas(color string) js.Object {
+func generateGlyphCanvas(color string) *js.Object {
 	glyphCanvas := js.Global.Get("document").Call("createElement", "canvas")
 	glyphCanvas.Set("width", int(glyphCellSize*glyphsCols+glyphCellSize))
 	glyphCanvas.Set("height", int(glyphCellSize*glyphsCount/glyphsCols+glyphCellSize))
@@ -328,10 +339,9 @@ func generateGlyphCanvas(color string) js.Object {
 			fontSize *= .87
 		}
 		cellSize = cellSize
-		//ctx.Call("translate", cellSize*float64(col)+cellSize/2, cellSize*float64(row)+(fontSize-cellSize))
 		ctx.Call("save")
 		ctx.Set("textAlign", "center")
-		ctx.Set("font", fmt.Sprintf("%dpx Monaco, Helvetica, Arial, Sans-Serif", int(fontSize)))
+		ctx.Set("font", itoa(int(fontSize))+"px Monaco, Helvetica, Arial, Sans-Serif")
 		ctx.Set("shadowColor", color)
 		ctx.Set("shadowBlur", float64(fontSize)*.60)
 		switch c {
@@ -343,7 +353,6 @@ func generateGlyphCanvas(color string) js.Object {
 		}
 		for i := 0; i < 3; i++ {
 			ctx.Set("fillStyle", color)
-			//ctx.Call("fillText", string(c), cellSize*float64(col)+cellSize/2, cellSize*float64(row)+(fontSize-cellSize))
 			ctx.Call("fillText", string(c), 0, 0)
 
 		}
